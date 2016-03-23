05dfeca40fe383b62fc54fb8f5953701fc5567fc
XK <XK@xkdemac.local>
2016-03-23 20:12:37 +0800

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by XK on 16/3/23.
+//  Copyright © 2016年 XK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by XK on 16/3/23.
+//  Copyright © 2016年 XK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
