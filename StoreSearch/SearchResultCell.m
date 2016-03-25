6d9141054d3b6d1311fc51eca3c0d792df78c83f
xjkyo <xjkyo@126.com>
2016-03-25 21:59:10 +0800

    Styled the search bar and added the icon.

--- StoreSearch/SearchResultCell.m
+++ StoreSearch/SearchResultCell.m
@@ -11,7 +11,10 @@
 @implementation SearchResultCell
 
 - (void)awakeFromNib {
-    // Initialization code
+    [super awakeFromNib];
+    UIView *selectedView=[[UIView alloc]initWithFrame:CGRectZero];//同理:cell被tableview引用的时候会默认和tableview的宽度一样
+    selectedView.backgroundColor=[UIColor colorWithRed:20/255.0f green:160/255.0f blue:160/255.0f alpha:0.5f];
+    self.selectedBackgroundView=selectedView;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
