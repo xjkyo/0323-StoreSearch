531b3ff2d8e089368b06cc3545efa21df87ac231
xjkyo <xjkyo@126.com>
2016-04-01 13:47:59 +0800

    UIImageView+AFNetworking

--- StoreSearch/SearchResultCell.m
+++ StoreSearch/SearchResultCell.m
@@ -7,6 +7,8 @@
 //
 
 #import "SearchResultCell.h"
+#import "SearchResult.h"
+#import <AFNetworking/UIImageView+AFNetworking.h>
 
 @implementation SearchResultCell
 
@@ -17,10 +19,49 @@
     self.selectedBackgroundView=selectedView;
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+-(void)prepareForReuse{
+    [super prepareForReuse];
+    [self.artworkImageView cancelImageDownloadTask];
+    self.nameLabel=nil;
+    self.artistNameLabel=nil;
+}
 
-    // Configure the view for the selected state
+-(void)configureForSearchResult:(SearchResult *)searchResult{
+    self.nameLabel.text=searchResult.name;
+    //cell.artistNameLabel.text=searchResult.artistName;
+    NSString *artistName=searchResult.artistName;
+    if (artistName == nil) {
+        artistName=@"Unknown";
+    }
+    NSString *kind=[self kindForDisplay:searchResult.kind];
+    self.artistNameLabel.text=[NSString stringWithFormat:@"%@ (%@)",artistName,kind];
+    [self.artworkImageView setImageWithURL:[NSURL URLWithString:searchResult.artworkURL60] placeholderImage:[UIImage imageNamed:@"Placeholder"]];
+}
+
+-(NSString *)kindForDisplay:(NSString *)kind{
+    if ([kind isEqualToString:@"album"]) {
+        return @"Album";
+    }else if ([kind isEqualToString:@"audiobook"]){
+        return @"Audio Book";
+    }else if ([kind isEqualToString:@"book"]){
+        return @"Book";
+    }else if ([kind isEqualToString:@"ebook"]){
+        return @"E-Book";
+    }else if ([kind isEqualToString:@"feature-movie"]){
+        return @"Movie";
+    }else if ([kind isEqualToString:@"music-video"]){
+        return @"Music Video";
+    }else if ([kind isEqualToString:@"podcast"]){
+        return @"Podcase";
+    }else if ([kind isEqualToString:@"software"]){
+        return @"App";
+    }else if ([kind isEqualToString:@"song"]){
+        return @"Song";
+    }else if ([kind isEqualToString:@"tv-episode"]){
+        return @"TV Episode";
+    }else{
+        return kind;
+    }
 }
 
 @end
--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -81,41 +81,8 @@ static NSString * const LoadingCellIdentifier=@"LoadingCell";
     }else{
         SearchResultCell *cell = (SearchResultCell *)[tableView dequeueReusableCellWithIdentifier:SearchResultCellIdentifier forIndexPath:indexPath];
         SearchResult *searchResult=_searchResults[indexPath.row];
-        cell.nameLabel.text=searchResult.name;
-        //cell.artistNameLabel.text=searchResult.artistName;
-        NSString *artistName=searchResult.artistName;
-        if (artistName == nil) {
-            artistName=@"Unknown";
-        }
-        NSString *kind=[self kindForDisplay:searchResult.kind];
-        cell.artistNameLabel.text=[NSString stringWithFormat:@"%@ (%@)",artistName,kind];
+        [cell configureForSearchResult:searchResult];
         return cell;
-    }
-}
-
--(NSString *)kindForDisplay:(NSString *)kind{
-    if ([kind isEqualToString:@"album"]) {
-        return @"Album";
-    }else if ([kind isEqualToString:@"audiobook"]){
-        return @"Audio Book";
-    }else if ([kind isEqualToString:@"book"]){
-        return @"Book";
-    }else if ([kind isEqualToString:@"ebook"]){
-        return @"E-Book";
-    }else if ([kind isEqualToString:@"feature-movie"]){
-        return @"Movie";
-    }else if ([kind isEqualToString:@"music-video"]){
-        return @"Music Video";
-    }else if ([kind isEqualToString:@"podcast"]){
-        return @"Podcase";
-    }else if ([kind isEqualToString:@"software"]){
-        return @"App";
-    }else if ([kind isEqualToString:@"song"]){
-        return @"Song";
-    }else if ([kind isEqualToString:@"tv-episode"]){
-        return @"TV Episode";
-    }else{
-        return kind;
     }
 }
 
